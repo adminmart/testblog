4ac087843830249a5b42f58f259aaf942220782b
niravjoshi <niravjoshi87@gmail.com>
2023-11-24 19:18:54 +0530

    ''

--- app/blogs/[slug]/page.js
+++ app/blogs/[slug]/page.js
@@ -3,16 +3,18 @@ import { join } from "path";
 import fs from "fs";
 import matter from "gray-matter";
 
-export default function Page({ params }) {
+const BlogPost = ({ params }) => {
   const slugname = params.slug;
+  console.log(params);
   const postsPath = "blogsfiles";
-  const postFilePath = join(postsPath, `${slugname}.md`);
+  const postFilePath = join("blogsfiles", `/${slugname}.md`);
   const fileContents = fs.readFileSync(postFilePath, "utf8");
 
   // const fileContents = () => fs.readFileSync(require.resolve(postFilePath), { encoding: "utf8" });
 
   const matterResult = matter(fileContents);
-  console.log("newsitem", matterResult);
 
   return <div>My Post: {matterResult.data.title}</div>;
-}
+};
+
+export default BlogPost;
