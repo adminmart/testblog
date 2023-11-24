3baebfca7e41fa79629f038ca2debea1abc90040
niravjoshi <niravjoshi87@gmail.com>
2023-11-24 20:04:40 +0530

    ''

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["adminmart.com", "www.wrappixel.com", "adminmart.github.io"],
-  },
-};
-
-module.exports = nextConfig;
